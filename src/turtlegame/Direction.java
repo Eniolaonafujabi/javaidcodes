54d7d31c52de21e81677945370b2a4831e9b466d
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-07-07 10:04:07 +0100

    work

---
+++ src/turtlegame/Direction.java
@@ -0,0 +1,10 @@
+package turtlegame;
+
+public enum Direction {
+    WEST, SOUTH, North, EAST;
+}
+//public  boolean equals(Object pos) {
+//    Direction enteredPosition = (Direction) pos;
+//    return false;
+//}
+
