7f7e83c2d2f0ef5da344e1639e5d33504911b17e
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-07-07 10:05:02 +0100

    work

---
+++ src/turtlegame/Position.java
@@ -0,0 +1,36 @@
+package turtlegame;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public void changeRowPosition(int step,Direction direction) {
+       if(Direction.SOUTH == direction)row+=step;
+       else if (Direction.North==direction)row-=step;
+    }
+
+    public void changeColumnPosition(int step, Direction direction) {
+        if (Direction.WEST==direction)column-=step;
+        else if (Direction.EAST==direction)column+=step;
+    }
+
+    @Override
+    public boolean equals(Object pos) {
+        Position enteredPosition = (Position) pos;
+        return this.row == enteredPosition.row && this.column == enteredPosition.column;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "row=" + row +
+                ", column=" + column +
+                '}';
+    }
+}
