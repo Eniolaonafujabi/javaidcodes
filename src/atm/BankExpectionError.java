ba04d66ce8a9a716d93e508fdd9723830238429d
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-07-16 15:31:17 +0100

    work

---
+++ src/atm/BankExpectionError.java
@@ -0,0 +1,9 @@
+package atm;
+
+public class BankExpectionError extends Exception {
+
+
+    public String errorMessageForWrongPin(String message){
+        return message;
+    };
+}
--- test/atm/BankTest.java
+++ test/atm/BankTest.java
@@ -1,4 +1,4 @@
-package tdd;
+package atm;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
